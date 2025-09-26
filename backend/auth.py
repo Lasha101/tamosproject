7c5f7a0777d7095ffc84a1ef275a99f43bee8b77
Lasha101 <gurshabo55@gmail.com>
2025-09-27 00:45:53 +0400

    refresh

--- backend/auth.py
+++ backend/auth.py
@@ -11,7 +11,7 @@ from database import get_db
 
 # --- Configuration ---
 # Load from environment variables for security
-SECRET_KEY = os.getenv("SECRET_KEY", "a_default_secret_key_for_development")
+SECRET_KEY = "tamostratoradmin"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
